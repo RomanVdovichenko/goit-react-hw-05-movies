811168f9374fe17ee21056c8f694d7e7bc0fb992
RomanVdovichenko <104017963+RomanVdovichenko@users.noreply.github.com>
2023-06-13 02:02:34 +0000

    Deploying to gh-pages from @ RomanVdovichenko/goit-react-hw-05-movies@7dc19fb4d0a50cd97fcf141e6b79d31b3fdb40be 🚀

---
+++ static/js/795.cad4051c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[795],{5795:function(e,t,_){_.r(t),t.default={reviews:"Reviews_reviews__XF4LF",text:"Reviews_text__FXlU7"}}}]);
+//# sourceMappingURL=795.cad4051c.chunk.js.map
